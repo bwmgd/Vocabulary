f2aa5705f3b891f42cee5971c42b16e73fb1fbb8
bwmgd <1971138851@qq.com>
2020-10-10 13:11:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/vocabulary/sqlite/WordsDBHelper.java
@@ -0,0 +1,43 @@
+package com.example.vocabulary.sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.vocabulary.dummy.Words;
+
+public class WordsDBHelper extends SQLiteOpenHelper {
+
+    private final static String DATABASE_NAME = "wordsDB";//数据库名字
+    private final static int DATABASE_VERSION = 1;//数据库版本
+    //建表语句
+    private final static String SQL_CREATE_DATABASE = "CREATE TABLE "
+            + Words.Word.TABLE_NAME + " (" +
+            Words.Word._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            Words.Word.COLUMN_NAME_WORD + " TEXT" + "," +
+            Words.Word.COLUMN_NAME_MEANING + " TEXT" + ","
+            + Words.Word.COLUMN_NAME_SAMPLE + " TEXT" + " )";
+
+    //删表语句
+    private final static String SQL_DELETE_DATABASE = "DROP TABLE IF EXISTS " + Words.Word.TABLE_NAME;
+
+
+    public WordsDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    //建表
+    public void onCreate(SQLiteDatabase db) {
+        Log.v("test", "建表成功");
+        db.execSQL(SQL_CREATE_DATABASE);
+    }
+
+    @Override
+    //更新，先删后建
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_DATABASE);
+        onCreate(db);
+    }
+}
